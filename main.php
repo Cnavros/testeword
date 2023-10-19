7f8b4048365e437987ae0e53f48a4f4dfeec09fa
Cnavros <cnavros@gmail.com>
2023-10-19 09:49:27 -0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,2 @@
+<h2>Conteúdo Principal</h2>
+<p>Este é um exemplo de conteúdo dinâmico em uma seção "main" no seu site PHP. Você pode incluir este arquivo em outras páginas para exibir conteúdo específico.</p>
